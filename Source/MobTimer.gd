ed55073d230b3f1309b605f03e2e6ddbfc8c4808
[psmclancy] <[psmclancy@gmail.com]>
2023-11-18 12:31:35 -0800

    Adding exported variable for spawn speed increase

--- Source/MobTimer.gd
+++ Source/MobTimer.gd
@@ -1,11 +1,9 @@
-
 extends Timer
 
-
-
-
+@export var speed_increase = .01
 
 func _on_reduce_timer_timeout():
 	if wait_time > .05 :
-		wait_time -= .01
+		wait_time -= speed_increase
 		print ("wait time", wait_time)
+
