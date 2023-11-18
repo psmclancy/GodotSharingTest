a43e9193ac7c5b64c1d9940f90d619fe1ac8e317
[psmclancy] <[psmclancy@gmail.com]>
2023-11-18 11:58:43 -0800

    Adding mob spawning

--- Source/mob.gd
+++ Source/mob.gd
@@ -4,7 +4,7 @@ extends CharacterBody3D
 @export var min_speed = 10
 @export var max_speed = 18
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	move_and_slide()
 	
 func initialize(start_position, player_position):
